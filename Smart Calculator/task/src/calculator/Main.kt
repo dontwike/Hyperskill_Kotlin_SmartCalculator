e4f01b9d76df6fe8a55134177a50c39772128073
BRDLF <adfassl2@gmail.com>
2022-09-07 15:28:45 -0500

    Completed Stage 2/8. added control loop. Exit command added. Exit message added.

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -4,16 +4,32 @@ import kotlin.system.exitProcess
 
 object Calculator {
 
-    fun add() {
-        val inputArray = readln().trim().split(" ").map {it.toIntOrNull()?: throw IllegalArgumentException("\"$it\" is not an integer.")}
-        val (firstInt, secondInt) = if (inputArray.size == 2) inputArray else throw IllegalArgumentException("add() was passed the wrong number of integers.")
-        println(firstInt + secondInt)
+    fun start() {
+        do {
+            when (val input = readln().trim()) {
+                "/exit" -> break
+                "" -> continue
+                else -> processInput(input)
+            }
+        } while (true)
+        println("Bye!")
+    }
+
+    private fun processInput(input: String) {
+        val operands = input.split(" ").map {it.toIntOrNull()?: throw IllegalArgumentException("\"$it\" is not an integer.")}
+        add(operands)
+    }
+
+    private fun add(operands: List<Int>) {
+        if (operands.size > 2) throw IllegalArgumentException("add() was passed the wrong number of integers.")
+        val sum = operands.sumOf { it }
+        println(sum)
     }
 }
 
 fun main() {
     try {
-        Calculator.add()
+        Calculator.start()
     } catch (e: RuntimeException) {
         println(e.message)
         exitProcess(1)
