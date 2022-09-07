653ab053c969317205279cf80b5439d7ed98f426
BRDLF <adfassl2@gmail.com>
2022-09-07 18:54:59 -0500

    Completed stage 4/8. Updated Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -31,3 +31,25 @@ But I figure it's easier to parse this way.
 
 Well this is a good example of the benefits of thinking ahead.
 Added a single-line function for a '\help' command and removed the single-line restriction on the add function.
+
+### Stage 4/8: Add subtractions
+
+This gave me more trouble than expecting.
+Now that a valid input is no longer space separated integers, but full phrases with operators and operands, I con't resort on MutableList<Int>.sumOf {it}
+I needed to make new functions to parse the input string, find the operators, and then perform the associated function.
+Eliminating reduntant operators was simple enough with String.replace(), but what gave me trouble was the actual function I used to reduce an input string to a result.
+Originally I thought it was best to run .math() recursively.
+    1. Find a valid operator
+    2. Split the input string into left and right MutableList<String>s
+    3. Depending on the operator, call the appropriate function on left.math() and right.math(), 
+    4. .math() would then call recursively until each list of Strings became a single string.
+    
+But calling recursively like that doesn't actually follow the rules of... Maths.
+So I went back and just used a while loop to iterably go over the input list until it's reduced to a single value
+I don't like how right now i'm using
+```
+this[operatorIndex] = result.toString()
+this.removeAt(operatorIndex + 1)
+this.removeAt(operatorIndex - 1)
+```
+But it works. More importantly, it should allow for multiplication & division to be added easily in the future.
--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -10,7 +10,9 @@ object Calculator {
                 "/exit" -> break
                 "/help" -> showHelp()
                 "" -> continue
-                else -> processInput(input)
+                else -> {
+                    println(input.split(" ").filter { it != "" }.toMutableList().math())
+                }
             }
         } while (true)
         println("Bye!")
@@ -18,15 +20,42 @@ object Calculator {
 
     private fun showHelp() = println("The program calculates the sum of numbers")
 
-    private fun processInput(input: String) {
-        val operands = input.split(" ").map {it.toIntOrNull()?: throw IllegalArgumentException("\"$it\" is not an integer.")}
-        add(operands)
+    private fun MutableList<String>.math(): String {
+        while (this.size != 1) {
+            if (this.size % 2 == 0) throw IllegalArgumentException("Invalid number of inputs")
+            val operatorIndex = this.findOperator()
+            val left = this[operatorIndex - 1].toIntOrNull()?: throw Exception("${this[operatorIndex - 1]} is not a valid input")
+            val right = this[operatorIndex + 1].toIntOrNull()?: throw Exception("${this[operatorIndex + 1]} is not a valid input")
+            val operator = this[operatorIndex]
+
+            val result = when (operator) {
+                "+" -> left + right
+                "-" -> left - right
+                else -> throw IllegalArgumentException("found invalid operator")
+            }
+
+            this[operatorIndex] = result.toString()
+            this.removeAt(operatorIndex + 1)
+            this.removeAt(operatorIndex - 1)
+        }
+        return this[0]
     }
 
-    private fun add(operands: List<Int>) {
-        val sum = operands.sumOf { it }
-        println(sum)
+    private fun MutableList<String>.findOperator(): Int {
+        if (this[1].contains("+") || this[1].contains("-")) {this.simplifyOperator(1)}
+        return 1
     }
+
+    private fun MutableList<String>.simplifyOperator(pos: Int) {
+        var workingString = this[pos]
+        while (workingString.length != 1) {
+            if (workingString[0] != '+' && workingString[0] != '-') throw IllegalArgumentException("Invalid operator: ${this[pos]}")
+            workingString = workingString.simplified()
+        }
+        this[pos] = workingString
+    }
+    private fun String.simplified(): String = this.replace("--", "+").replace("++", "+")
+        .replace("+-", "-").replace("-+", "-")
 }
 
 fun main() {
